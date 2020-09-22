195d4b0cceb119b74464e866d49745724e2fb6e6
gwagsi <gwagsiglenn@gmail.com>
2020-09-22 02:29:37 +0100

    Creating component structure

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./assets/App.css";
 import Header from "./components/Header";
 import LeftArrow from "./components/Carousel/left_arrow";
 import RightArrow from "./components/Carousel/right_arrow";
+import Slider from "./components/carousel/Slider";
+import Category from "./components/Category";
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Hero*/}
-      <LeftArrow />
-      <RightArrow />
+
+      <Slider />
+      <Category />
       {/* Category -card*/}
       {/*Deals cardv2 */}
       {/* Celebrate[category]*/}
